94dd97b579b8f53229f2c2adb2e715991b440372
Caleb Heydon <caleb.heydon@gmail.com>
2023-03-04 14:08:49 -0500

    Update Drivetrain.cxx

--- src/main/cpp/lib173/Drivetrain.cxx
+++ src/main/cpp/lib173/Drivetrain.cxx
@@ -11,8 +11,6 @@
 #include "Constants.hxx"
 #include "lib173/StateEstimator.hxx"
 
-#include <iostream>
-
 Drivetrain::Drivetrain()
 {
     mKinematics = std::make_shared<frc::DifferentialDriveKinematics>(units::meter_t{Constants::kDrivetrainWidth});
@@ -45,7 +43,7 @@ void Drivetrain::driveVelocity(double left, double right)
 void Drivetrain::update(double timestamp)
 {
     mTrajectoryMutex.lock();
-    std::cout << mTrajectory << "\n";
+
     std::shared_ptr<StateEstimator> stateEstimator = StateEstimator::instance();
 
     if (mPathFollowingTimestamp < 0 && mTrajectory)
